f5df0979b440645a2e2e553210125db6e899d1e7
IoIxD <alphaproject217@gmail.com>
2025-05-28 15:02:33 -0700

    windows compat? for the rumble example

--- examples/gamepadrumble.c
+++ examples/gamepadrumble.c
@@ -1,17 +1,28 @@
 #include "minigamepad.h"
-#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 
+#ifndef __WIN32
+#include <pthread.h>
+#else
+#include <windows.h>
+#endif
+
 mg_gamepads *gamepads;
 mg_gamepad *gamepad;
-pthread_t tid;
+
 double axis_value;
 
+#ifndef __WIN32
+pthread_t thread_id;
 void *rumble_thread(void *arg);
+#else
+HANDLE thread_handle;
+DWORD WINAPI rumble_thread(LPVOID lpParam);
+#endif
 
 int main(void) {
   gamepads = mg_gamepads_get();
@@ -21,22 +32,43 @@ int main(void) {
   }
 
   gamepad = gamepads->list[0];
-  pthread_create(&tid, NULL, &rumble_thread, NULL);
 
+#ifndef __WIN32
+  pthread_create(&thread_id, NULL, &rumble_thread, NULL);
+#else
+  thread_handle =
+      CreateThread(NULL,                 // default security attributes
+                   0,                    // use default stack size
+                   rumble_thread,        // thread function name
+                   pDataArray[i],        // argument to thread function
+                   0,                    // use default creation flags
+                   &dwThreadIdArray[i]); // returns the thread identifier
+#endif
   for (;;) {
     mg_gamepad_update(gamepad);
     axis_value += (double)gamepad->axises[0].value / 1000000000.0;
   }
 
   mg_gamepads_free(gamepads);
+
+#ifndef __WIN32
+  pthread_cancel(thread_id);
+#else
+  CloseHandle(thread_id);
+#endif
+
   return 0;
 }
 
+#ifndef __WIN32
 void *rumble_thread(void *arg) {
+#else
+DWORD WINAPI rumble_thread(LPVOID lpParam) {
+#endif
   while (true) {
     usleep(100);
 
-    mg_gamepad_rumble(gamepad, axis_value / 2, axis_value, 1);
+    mg_gamepad_rumble(gamepad, axis_value / 2, axis_value, 100);
     printf("\rVibration: %0.2f", axis_value);
   }
 
